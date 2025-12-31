78187349684843808a678147c2b68a3b96bc8eab
Aung Myo Kyaw <athuyrain@gmail.com>
2025-12-31 10:09:09 +0630

    Add script to create and set up React project
    add bash

---
+++ bash.sh
@@ -0,0 +1,8 @@
+# Create new React project with Vite
+npm create vite@latest chatapp-3d --template react
+
+# Navigate and install dependencies
+cd chatapp-3d
+npm install three @react-three/fiber @react-three/drei
+npm install firebase react-router-dom zustand tailwindcss
+npm install -D autoprefixer postcss
